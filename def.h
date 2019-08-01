3f3514596e90fca4f8a7ea9af0dcbe862d9572e7
wanguangming <156530445@qq.com>
2019-08-02 00:38:34 +0800

    resize bg png

--- def.h
+++ def.h
@@ -14,7 +14,7 @@
 #define NB_VISIBLE_LINES      14
 #define Y_LIST                17
 #define Y_HEADER              3
-#define Y_FOOTER              228
+#define Y_FOOTER              308
 #define H_FOOTER              13
 // Dialogs
 #define DIALOG_BORDER         2
